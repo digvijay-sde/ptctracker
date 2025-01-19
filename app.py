018a7a59342dd96cfc5a2186519af27b691a1a45
Digvijay Patil <workfordvp@gmail.com>
2025-01-20 03:52:51 +0530

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -76,4 +76,7 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
